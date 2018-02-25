c1baf7e423b33f42566f382cd42032237a129df8
Andrew Papacostas <a_papacostas@yahoo.com>
2018-02-25 06:50:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+ function distanceFromHqInBlocks(blockNum) {
+  if (blockNum > 42) {
+    return blockNum - 42
+  } else {
+    return 42 - blockNum
+  }
+ }
+
+function distanceFromHqInFeet(blockNum) {
+  return distanceFromHqInBlocks(blockNum) * 264
+}
+
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * distance;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
