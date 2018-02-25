8dbcc07bf1597c1ea0f1f0e9addae7467525047f
Andrew Papacostas <a_papacostas@yahoo.com>
2018-02-25 06:33:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Code your solution in this file!
  function distanceFromHqInBlocks(pickNum) {
-  //  if (pickNum === 1)
-  //  return pickNum
+  if (pickNum > 42) {
+    return pickNum - 42
+  } else {
+    return 42 - pickNum
+  }
  }
